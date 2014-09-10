e22fe8566ddeee12f4f60cafea10f95f1d2f9fd5
LitleDragon <maledictionemaeternitatis@gmail.com>
2014-09-10 18:48:39 +1000

    <final>

--- Interactive Questionaire/Reader.js
+++ Interactive Questionaire/Reader.js
@@ -15,13 +15,13 @@ var _Total_Score = 0;
 var _Choice_selected = "";
 
 // Need a variable to keep track of the number of questions created. This can track each one answered and therefore keep it on screen...
-var QuestionNumber= 0;
+var _QuestionNumber= 1;
 
 // Also need a variable to show the end
 var end = false;
 
 // next Question Temporary, Input first question here
-var _nextQuestion = 'fuck';
+var _nextQuestion = 'Question_1';
 
 //check
 
@@ -46,8 +46,8 @@ var quickread=function (obj) {
 
 // add_Choice will increase the number_of_choices by 1 and also assign that choice with the parameters
 var add_Choice = function (obj,text,score,link,_remark) {
-    obj['choice'+obj.number_of_choices]=[obj.number_of_choices,text,score,link,_remark];
-    obj.number_of_choices = obj.number_of_choices+1;
+    _Questions[obj]['choice'+_Questions[obj].number_of_choices]=[_Questions[obj].number_of_choices,text,score,link,_remark];
+    _Questions[obj].number_of_choices = _Questions[obj].number_of_choices+1;
 
 };
 
@@ -64,7 +64,7 @@ var return_Question = function (_signature) {
 };
 
 
-// Answer Question, here is teh meat of the thing
+// Answer Question, here is the meat of the thing
 // _Choose_Choice will record the choice given, the score given and the next question if applicable.
 // The link will either be "false" or a text that will search for the question with their identifier
 // This should also add the score in with the total... which I havnt made oops. Better make it inside this object?
@@ -72,7 +72,6 @@ var return_Question = function (_signature) {
 
 var _Choose_Choice = function (identifier,choice) {
     return _Questions[identifier]['choice'+choice];
-
 };
 
 // The following functions return stuff as strings that are useful
@@ -80,7 +79,7 @@ var return_Question_as_String = function(_signature) {
     return _Questions[_signature]['question_text'];
 };
 
-var return_Question_NumberofChoices=function(Question,choice){
+var return_Question_NumberofChoices=function(Question){
     return _Questions[Question].number_of_choices;
 };
 
@@ -105,52 +104,91 @@ var return_score_Choice_Number=function(Question,choice){
 
 // HTML
 
-//Create Question
+//Create Question, adds a question and its choices into the main html
 var _Create_HTML_Question = function () {
     var area = document.createElement("div");
     var node = document.createTextNode("");
     area.appendChild(node);
     area.className="QuestionArea";
-    area.id="Question"+QuestionNumber;
+    area.id="Question"+_QuestionNumber;
     var element = document.getElementById("main");
     element.appendChild(area);
     var thingy = document.createElement("div");
-    var addtothingy = document.createTextNode(return_Question_as_String(_nextQuestion));
+    var addtothingy = document.createTextNode(_QuestionNumber+". "+return_Question_as_String(_nextQuestion));
     thingy.appendChild(addtothingy);
     thingy.className = "Question";
     thingy.id = return_Question(_nextQuestion);
     area.appendChild(thingy);
-    for (var i=1;i<20;i++) {
+    for (var i=1;i<return_Question_NumberofChoices(_nextQuestion);i++) {
         var added = document.createElement("div");
-        var add = document.createTextNode(return_text_Choice_Number(_nextQuestion, i));
+        var add = document.createTextNode(i+". "+return_text_Choice_Number(_nextQuestion, i));
         added.appendChild(add);
         added.className = "Choice";
-        added.id = "Choice"+i+"_of_" + QuestionNumber;
+        added.id = "Choice"+i+"_of_" + _QuestionNumber;
         area.appendChild(added);
     }
 };
 
+var _Create_HTML_Remark = function() {
+    var area = document.createElement("div");
+    var node = document.createTextNode(return_remark_Choice_Number(_nextQuestion,_Choice_selected));
+    area.appendChild(node);
+    area.className="Choice";
+    area.id="Remark"+_QuestionNumber;
+    var element = document.getElementById("main");
+    element.appendChild(area);
+};
 
+
+var _Create_HTML_End = function() {
+    var area = document.createElement("div");
+    var node = document.createTextNode(_Total_Score);
+    area.appendChild(node);
+    area.className="Score";
+    area.id="FinalScore"+_QuestionNumber;
+    var element = document.getElementById("main");
+    element.appendChild(area);
+};
 /* test */
 
-add_Question('niggers','yeahhhhhh bruhhhhh');
+//deleted for profanity
 
-add_Question('fuck','get money');
+// Actual test Questions below
 
-add_Choice(_Questions.fuck,'eat shit lol',0,false,"You are a piece of shit");
+//Question 1
+add_Question("Question_1","Test Question 1");
+add_Choice("Question_1","Link to Question 1",1,"Question_1","Question 1 going to 1");
+add_Choice("Question_1","Link to Question 2",2,"Question_2","Question 1 going to 2");
+add_Choice("Question_1","Link to Question 3",3,"Question_3","Question 1 going to 3");
+add_Choice("Question_1","Link to Question 4",4,"Question_4","Question 1 going to 4");
+add_Choice("Question_1","end",-100000000,"Question 2","Goodbye");
+//Question 2
+add_Question("Question_2","Test Question 2");
 
-quickread(_Questions.niggers);
+add_Choice("Question_2","Link to Question 1",1,"Question_1","Question 2 going to 1");
+add_Choice("Question_2","Link to Question 2",2,"Question_2","Question 2 going to 2");
+add_Choice("Question_2","Link to Question 3",3,"Question_3","Question 2 going to 3");
+add_Choice("Question_2","Link to Question 4",4,"Question_4","Question 2 going to 4");
+add_Choice("Question_2","end",-100000000,"Question 2","Goodbye");
 
-add_Choice(_Questions["fuck"],'Noyoucant',0,false,"Crassshh");
+//Question 3
+add_Question("Question_3","Test Question 3");
+add_Choice("Question_3","Link to Question 1",1,"Question_1","Question 3 going to 1");
+add_Choice("Question_3","Link to Question 2",2,"Question_2","Question 3 going to 2");
+add_Choice("Question_3","Link to Question 3",3,"Question_3","Question 3 going to 3");
+add_Choice("Question_3","Link to Question 4",4,"Question_4","Question 3 going to 4");
+add_Choice("Question_3","end",-100000000,"Question 2","Goodbye");
 
-add_Choice(_Questions.niggers,'Chiikkeennn',2,false,"go fuck urself");
+//Question 4
+add_Question("Question_4","Test Question 4");
+add_Choice("Question_4","Link to Question 1",1,"Question_1","Question 4 going to 1");
+add_Choice("Question_4","Link to Question 2",2,"Question_2","Question 4 going to 2");
+add_Choice("Question_4","Link to Question 3",3,"Question_3","Question 4 going to 3");
+add_Choice("Question_4","Link to Question 4",4,"Question_4","Question 4 going to 4");
+add_Choice("Question_4","end",-100000000,"Question 2","Goodbye");
 
-quickread(_Questions.niggers);
-quickread(_Questions.fuck);
-
-console.log(return_Question('fuck'));
-
-console.log(_Choose_Choice('niggers',1));
+// Okay thats all folks!
+add_Question("Question 1","Test Question 1");
 
 document.getElementById('Button').onclick = function() {
     _Create_HTML_Question();
@@ -159,3 +197,22 @@ document.getElementById('Button').onclick = function() {
 document.getElementById('update').onclick = function() {
     _nextQuestion = document.getElementById('nextone').value;
 };
+document.getElementById('choosechoice').onclick = function() {
+    _Choice_selected = document.getElementById('choicenumber').value;
+    _Create_HTML_Remark();
+
+    _Total_Score+=return_score_Choice_Number(_nextQuestion,_Choice_selected);
+
+    if (return_link_Choice_Number(_nextQuestion,_Choice_selected) === false ){
+        alert("end");
+        //_Create_HTML_End();
+    }
+    else {
+        _nextQuestion=return_link_Choice_Number(_nextQuestion,_Choice_selected);
+        _QuestionNumber++;
+        _Create_HTML_Question();
+    }
+};
+
+//Start:
+_Create_HTML_Question();
